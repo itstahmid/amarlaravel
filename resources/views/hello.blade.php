2427aea76d5198fa75dd766516f13f620fa7d4de
itstahmid <iamtahmid.bd@gmail.com>
2023-05-15 16:15:24 +0600

    New Release

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Hello Page</title>
+  <style>
+    /* Reset default margin and padding */
+    * {
+      margin: 0;
+      padding: 0;
+    }
+    
+    /* Set full-height for the page */
+    html, body {
+      height: 100%;
+    }
+    
+    /* Center content vertically */
+    body {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background-color: #f2f2f2;
+      font-family: Arial, sans-serif;
+    }
+    
+    /* Welcome message */
+    .welcome {
+      text-align: center;
+      padding: 20px;
+      background-color: #ffffff;
+      border-radius: 10px;
+      box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+    }
+    
+    /* Media query for responsiveness */
+    @media (max-width: 768px) {
+      .welcome {
+        max-width: 80%;
+      }
+    }
+  </style>
+</head>
+<body>
+  <!-- <div class="welcome"> -->
+    <h1>Hello Page</h1>
+  <!-- </div> -->
+</body>
+</html>
